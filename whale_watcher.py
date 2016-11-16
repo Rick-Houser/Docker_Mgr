508766abf685e0b54616340d55df15bad9299a06
FrackenToaster <frakentoaster@gmail.com>
2016-11-16 14:51:20 -0800

    create class structure

---
+++ whale_watcher.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+class WhaleWatcher:
+    def __init__(self):
+
+    def main(self):
+
+    def run(self):
+
+    def stop(self):
+
+    def clean_up(self):
+
+if __name__ == '__main__':
+    watcher = WhaleWatcher()
+    watcher.main()
