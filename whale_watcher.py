51536f0cd3df7fa5f5f8679f9dee823c1b6d3075
FrackenToaster <frakentoaster@gmail.com>
2016-11-16 14:54:46 -0800

    Add descriptive class comments

--- whale_watcher.py
+++ whale_watcher.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 
 class WhaleWatcher:
+    ''' A tool used to manage the execution of software in Docker containers '''
     def __init__(self):
 
     def main(self):
 
     def run(self):
+    ''' Runs a given command within a container '''
 
     def stop(self):
+    ''' Stops a container '''
 
     def clean_up(self):
+    ''' Remove exited containers '''
 
 if __name__ == '__main__':
     watcher = WhaleWatcher()
